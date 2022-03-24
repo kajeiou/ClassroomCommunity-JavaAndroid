659a1d00cbc37f5495b30beb7129734106fbba82
NABIL KAJEIOU <kajena14061@iutparis.local>
2022-03-24 17:04:22 +0100

    first commit

---
+++ app/src/main/java/paci/iut/LoginActivity.java
@@ -0,0 +1,14 @@
+package paci.iut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
